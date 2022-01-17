ffdf47063b031a8245801ae3ca057915d91a83ac
Strooom <pascal.roobrouck@gmail.com>
2022-01-17 12:56:19 +0100

    add example file

---
+++ examples/test/test.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include "TSC2046.h"
+
+constexpr uint32_t chipSelectPin{32U};        // On our hardware, this is GPIO 32, maybe different on other hardware setups
+TSC2046 theTouchScreen(chipSelectPin);        // create a global object holding the driver data
+
+void setup() {
+    Serial.begin(115200);
+    Serial.flush();
+    Serial.println("Testing TSC2046");
+}
+
+void loop() {
+    theTouchScreen.sample();
+
+    if (theTouchScreen.getIsTouched()) {
+        uint32_t x = theTouchScreen.getX();
+        uint32_t y = theTouchScreen.getY();
+        Serial.print("touched on x = ");
+        Serial.print(x);
+        Serial.print(", y = ");
+        Serial.println(y);
+    } else {
+        Serial.println("not touched");
+    }
+
+    delay(500);
+}
